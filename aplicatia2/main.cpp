45f8a92d70afce4f6a33bc3a8cb93903c6fc924d
Victor-Lucian <91419028+Victor-Lucian@users.noreply.github.com>
2022-02-07 11:39:11 +0200

    Update main.cpp

--- aplicatia2/main.cpp
+++ aplicatia2/main.cpp
@@ -5,24 +5,27 @@ using namespace std;
 
 /**
     Se citeste un numar natural n (1 <=n <=100) si o matrice patratica de dimensiune n X n,
-    din fisierul“xxx.mat”. Toate numerele din fisier au maxim 9 cifre.
+    din fisierulâ€œxxx.matâ€. Toate numerele din fisier au maxim 9 cifre.
     2. Sa se verifice daca exista macar o coloana ale carei elemente aflate in triunghiul din vest au
     toate acelasi numar de divizori primi.
 */
 
 int n;
-int a[n + 5][n + 5];
+int a[n][n];
 
 ifstream fin ("xxx.mat");
 
-void citire (int st, int dr)
+void citire(int i,int j)
 {
-    if (st == dr)
+    if(i==n)
         return;
-    fin >> a[st][dr];
-    int mij = (st + dr) / 2;
-    citire(st, mij);
-    citire(mij + 1, dr);
+    if(j==n)
+    {
+        citire(i+1,0);
+        return;
+    }
+    cin>>a[i][j];
+    citire(i,j+1);
 }
 
 
